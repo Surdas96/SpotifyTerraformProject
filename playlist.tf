3f9775e400678a74094bad690c2732913f340a63
Surdas Sutar <165639759+Surdas96@users.noreply.github.com>
2024-08-03 23:40:27 +0530

    Add files via upload

---
+++ playlist.tf
@@ -0,0 +1,4 @@
+ resource "spotify_playlist" "My_song_list" {
+      name = "My_song_list"
+      tracks = ["5fq7CGNYZKJm38HbLwMg9h"]
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+api_key = "adsHsWxUuEQ3mkusNf5GPfjG08J45-ahn2KkcidrMtlM5-C5E5KxWNz0SxyRmSHL"
+
